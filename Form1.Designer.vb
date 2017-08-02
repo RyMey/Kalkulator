c65a655b54f24bc2169a60a07a98ab02b9272e1a
Rya Meyvriska <rya.meyvriska@gmail.com>
2017-08-02 20:45:31 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,292 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TBLayar = New System.Windows.Forms.TextBox()
+        Me.BSatu = New System.Windows.Forms.Button()
+        Me.BDua = New System.Windows.Forms.Button()
+        Me.BTiga = New System.Windows.Forms.Button()
+        Me.BEnam = New System.Windows.Forms.Button()
+        Me.BLima = New System.Windows.Forms.Button()
+        Me.BEmpat = New System.Windows.Forms.Button()
+        Me.BSembilan = New System.Windows.Forms.Button()
+        Me.BDelapan = New System.Windows.Forms.Button()
+        Me.BTujuh = New System.Windows.Forms.Button()
+        Me.BNol = New System.Windows.Forms.Button()
+        Me.BKali = New System.Windows.Forms.Button()
+        Me.BKurang = New System.Windows.Forms.Button()
+        Me.BTambah = New System.Windows.Forms.Button()
+        Me.BBagi = New System.Windows.Forms.Button()
+        Me.BSamaDengan = New System.Windows.Forms.Button()
+        Me.BCe = New System.Windows.Forms.Button()
+        Me.BAc = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TBLayar
+        '
+        Me.TBLayar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TBLayar.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.TBLayar.Font = New System.Drawing.Font("Calibri", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TBLayar.Location = New System.Drawing.Point(7, 20)
+        Me.TBLayar.Name = "TBLayar"
+        Me.TBLayar.ReadOnly = True
+        Me.TBLayar.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TBLayar.Size = New System.Drawing.Size(225, 37)
+        Me.TBLayar.TabIndex = 0
+        Me.TBLayar.Text = "0"
+        Me.TBLayar.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'BSatu
+        '
+        Me.BSatu.Location = New System.Drawing.Point(7, 57)
+        Me.BSatu.Name = "BSatu"
+        Me.BSatu.Size = New System.Drawing.Size(40, 30)
+        Me.BSatu.TabIndex = 1
+        Me.BSatu.Text = "1"
+        Me.BSatu.UseVisualStyleBackColor = True
+        '
+        'BDua
+        '
+        Me.BDua.Location = New System.Drawing.Point(53, 57)
+        Me.BDua.Name = "BDua"
+        Me.BDua.Size = New System.Drawing.Size(40, 30)
+        Me.BDua.TabIndex = 2
+        Me.BDua.Text = "2"
+        Me.BDua.UseVisualStyleBackColor = True
+        '
+        'BTiga
+        '
+        Me.BTiga.Location = New System.Drawing.Point(100, 57)
+        Me.BTiga.Name = "BTiga"
+        Me.BTiga.Size = New System.Drawing.Size(40, 30)
+        Me.BTiga.TabIndex = 3
+        Me.BTiga.Text = "3"
+        Me.BTiga.UseVisualStyleBackColor = True
+        '
+        'BEnam
+        '
+        Me.BEnam.Location = New System.Drawing.Point(100, 93)
+        Me.BEnam.Name = "BEnam"
+        Me.BEnam.Size = New System.Drawing.Size(40, 30)
+        Me.BEnam.TabIndex = 6
+        Me.BEnam.Text = "6"
+        Me.BEnam.UseVisualStyleBackColor = True
+        '
+        'BLima
+        '
+        Me.BLima.Location = New System.Drawing.Point(53, 93)
+        Me.BLima.Name = "BLima"
+        Me.BLima.Size = New System.Drawing.Size(40, 30)
+        Me.BLima.TabIndex = 5
+        Me.BLima.Text = "5"
+        Me.BLima.UseVisualStyleBackColor = True
+        '
+        'BEmpat
+        '
+        Me.BEmpat.Location = New System.Drawing.Point(7, 93)
+        Me.BEmpat.Name = "BEmpat"
+        Me.BEmpat.Size = New System.Drawing.Size(40, 30)
+        Me.BEmpat.TabIndex = 4
+        Me.BEmpat.Text = "4"
+        Me.BEmpat.UseVisualStyleBackColor = True
+        '
+        'BSembilan
+        '
+        Me.BSembilan.Location = New System.Drawing.Point(100, 129)
+        Me.BSembilan.Name = "BSembilan"
+        Me.BSembilan.Size = New System.Drawing.Size(40, 30)
+        Me.BSembilan.TabIndex = 9
+        Me.BSembilan.Text = "9"
+        Me.BSembilan.UseVisualStyleBackColor = True
+        '
+        'BDelapan
+        '
+        Me.BDelapan.Location = New System.Drawing.Point(53, 129)
+        Me.BDelapan.Name = "BDelapan"
+        Me.BDelapan.Size = New System.Drawing.Size(40, 30)
+        Me.BDelapan.TabIndex = 8
+        Me.BDelapan.Text = "8"
+        Me.BDelapan.UseVisualStyleBackColor = True
+        '
+        'BTujuh
+        '
+        Me.BTujuh.Location = New System.Drawing.Point(7, 129)
+        Me.BTujuh.Name = "BTujuh"
+        Me.BTujuh.Size = New System.Drawing.Size(40, 30)
+        Me.BTujuh.TabIndex = 7
+        Me.BTujuh.Text = "7"
+        Me.BTujuh.UseVisualStyleBackColor = True
+        '
+        'BNol
+        '
+        Me.BNol.Location = New System.Drawing.Point(7, 165)
+        Me.BNol.Name = "BNol"
+        Me.BNol.Size = New System.Drawing.Size(133, 30)
+        Me.BNol.TabIndex = 10
+        Me.BNol.Text = "0"
+        Me.BNol.UseVisualStyleBackColor = True
+        '
+        'BKali
+        '
+        Me.BKali.Location = New System.Drawing.Point(146, 129)
+        Me.BKali.Name = "BKali"
+        Me.BKali.Size = New System.Drawing.Size(40, 30)
+        Me.BKali.TabIndex = 13
+        Me.BKali.Text = "x"
+        Me.BKali.UseVisualStyleBackColor = True
+        '
+        'BKurang
+        '
+        Me.BKurang.Location = New System.Drawing.Point(146, 93)
+        Me.BKurang.Name = "BKurang"
+        Me.BKurang.Size = New System.Drawing.Size(40, 30)
+        Me.BKurang.TabIndex = 12
+        Me.BKurang.Text = "-"
+        Me.BKurang.UseVisualStyleBackColor = True
+        '
+        'BTambah
+        '
+        Me.BTambah.Location = New System.Drawing.Point(146, 57)
+        Me.BTambah.Name = "BTambah"
+        Me.BTambah.Size = New System.Drawing.Size(40, 30)
+        Me.BTambah.TabIndex = 11
+        Me.BTambah.Text = "+"
+        Me.BTambah.UseVisualStyleBackColor = True
+        '
+        'BBagi
+        '
+        Me.BBagi.Location = New System.Drawing.Point(146, 165)
+        Me.BBagi.Name = "BBagi"
+        Me.BBagi.Size = New System.Drawing.Size(40, 30)
+        Me.BBagi.TabIndex = 14
+        Me.BBagi.Text = "÷"
+        Me.BBagi.UseVisualStyleBackColor = True
+        '
+        'BSamaDengan
+        '
+        Me.BSamaDengan.Location = New System.Drawing.Point(192, 129)
+        Me.BSamaDengan.Name = "BSamaDengan"
+        Me.BSamaDengan.Size = New System.Drawing.Size(40, 66)
+        Me.BSamaDengan.TabIndex = 16
+        Me.BSamaDengan.Text = "="
+        Me.BSamaDengan.UseVisualStyleBackColor = True
+        '
+        'BCe
+        '
+        Me.BCe.Location = New System.Drawing.Point(192, 57)
+        Me.BCe.Name = "BCe"
+        Me.BCe.Size = New System.Drawing.Size(40, 30)
+        Me.BCe.TabIndex = 17
+        Me.BCe.Text = "CE"
+        Me.BCe.UseVisualStyleBackColor = True
+        '
+        'BAc
+        '
+        Me.BAc.Location = New System.Drawing.Point(192, 93)
+        Me.BAc.Name = "BAc"
+        Me.BAc.Size = New System.Drawing.Size(40, 30)
+        Me.BAc.TabIndex = 18
+        Me.BAc.Text = "AC"
+        Me.BAc.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Label1.Location = New System.Drawing.Point(50, 203)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(130, 13)
+        Me.Label1.TabIndex = 19
+        Me.Label1.Text = "Rya Meyvriska - 4762"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.BAc)
+        Me.GroupBox1.Controls.Add(Me.BCe)
+        Me.GroupBox1.Controls.Add(Me.BSamaDengan)
+        Me.GroupBox1.Controls.Add(Me.BBagi)
+        Me.GroupBox1.Controls.Add(Me.BKali)
+        Me.GroupBox1.Controls.Add(Me.BKurang)
+        Me.GroupBox1.Controls.Add(Me.BTambah)
+        Me.GroupBox1.Controls.Add(Me.BNol)
+        Me.GroupBox1.Controls.Add(Me.BSembilan)
+        Me.GroupBox1.Controls.Add(Me.BDelapan)
+        Me.GroupBox1.Controls.Add(Me.BTujuh)
+        Me.GroupBox1.Controls.Add(Me.BEnam)
+        Me.GroupBox1.Controls.Add(Me.BLima)
+        Me.GroupBox1.Controls.Add(Me.BEmpat)
+        Me.GroupBox1.Controls.Add(Me.BTiga)
+        Me.GroupBox1.Controls.Add(Me.BDua)
+        Me.GroupBox1.Controls.Add(Me.BSatu)
+        Me.GroupBox1.Controls.Add(Me.TBLayar)
+        Me.GroupBox1.ForeColor = System.Drawing.SystemColors.GrayText
+        Me.GroupBox1.Location = New System.Drawing.Point(4, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(243, 231)
+        Me.GroupBox1.TabIndex = 20
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Calc"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(253, 239)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Kalkulator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TBLayar As System.Windows.Forms.TextBox
+    Friend WithEvents BSatu As System.Windows.Forms.Button
+    Friend WithEvents BDua As System.Windows.Forms.Button
+    Friend WithEvents BTiga As System.Windows.Forms.Button
+    Friend WithEvents BEnam As System.Windows.Forms.Button
+    Friend WithEvents BLima As System.Windows.Forms.Button
+    Friend WithEvents BEmpat As System.Windows.Forms.Button
+    Friend WithEvents BSembilan As System.Windows.Forms.Button
+    Friend WithEvents BDelapan As System.Windows.Forms.Button
+    Friend WithEvents BTujuh As System.Windows.Forms.Button
+    Friend WithEvents BNol As System.Windows.Forms.Button
+    Friend WithEvents BKali As System.Windows.Forms.Button
+    Friend WithEvents BKurang As System.Windows.Forms.Button
+    Friend WithEvents BTambah As System.Windows.Forms.Button
+    Friend WithEvents BBagi As System.Windows.Forms.Button
+    Friend WithEvents BSamaDengan As System.Windows.Forms.Button
+    Friend WithEvents BCe As System.Windows.Forms.Button
+    Friend WithEvents BAc As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,201 @@
+﻿Public Class Form1
+    Dim soalBersih As String = ""
+    Dim soal As String 
+    Dim kiri As String = ""
+    Dim kanan As String = ""
+    Dim hitung As Double
+    Sub cekAwal()
+        If TBLayar.Text = "0" Then
+            TBLayar.Text = ""
+        End If
+    End Sub
+    Function tautologi() As Boolean
+        If TBLayar.Text <> "0" And TBLayar.Text.Substring(TBLayar.Text.Length - 1, 1) <> "+" And TBLayar.Text.Substring(TBLayar.Text.Length - 1, 1) <> "-" And TBLayar.Text.Substring(TBLayar.Text.Length - 1, 1) <> "x" And TBLayar.Text.Substring(TBLayar.Text.Length - 1, 1) <> "÷" Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub BNol_Click(sender As Object, e As EventArgs) Handles BNol.Click
+        cekAwal()
+        TBLayar.Text += "0"
+    End Sub
+
+    Private Sub BSatu_Click(sender As Object, e As EventArgs) Handles BSatu.Click
+        cekAwal()
+        TBLayar.Text += "1"
+    End Sub
+
+    Private Sub BDua_Click(sender As Object, e As EventArgs) Handles BDua.Click
+        cekAwal()
+        TBLayar.Text += "2"
+    End Sub
+
+    Private Sub BTiga_Click(sender As Object, e As EventArgs) Handles BTiga.Click
+        cekAwal()
+        TBLayar.Text += "3"
+    End Sub
+
+    Private Sub BEmpat_Click(sender As Object, e As EventArgs) Handles BEmpat.Click
+        cekAwal()
+        TBLayar.Text += "4"
+    End Sub
+
+    Private Sub BLima_Click(sender As Object, e As EventArgs) Handles BLima.Click
+        cekAwal()
+        TBLayar.Text += "5"
+    End Sub
+
+    Private Sub BEnam_Click(sender As Object, e As EventArgs) Handles BEnam.Click
+        cekAwal()
+        TBLayar.Text += "6"
+    End Sub
+
+    Private Sub BTujuh_Click(sender As Object, e As EventArgs) Handles BTujuh.Click
+        cekAwal()
+        TBLayar.Text += "7"
+    End Sub
+
+    Private Sub BDelapan_Click(sender As Object, e As EventArgs) Handles BDelapan.Click
+        cekAwal()
+        TBLayar.Text += "8"
+    End Sub
+
+    Private Sub BSembilan_Click(sender As Object, e As EventArgs) Handles BSembilan.Click
+        cekAwal()
+        TBLayar.Text += "9"
+    End Sub
+
+    Private Sub BCe_Click(sender As Object, e As EventArgs) Handles BCe.Click
+        If (TBLayar.Text.Length = 1) Then
+            TBLayar.Text = "0"
+        Else
+            TBLayar.Text = TBLayar.Text.Substring(0, TBLayar.Text.Length - 1)
+        End If
+    End Sub
+
+    Private Sub BAc_Click(sender As Object, e As EventArgs) Handles BAc.Click
+        TBLayar.Text = "0"
+    End Sub
+
+    Private Sub BTambah_Click(sender As Object, e As EventArgs) Handles BTambah.Click
+        If tautologi() Then
+            TBLayar.Text += "+"
+        End If
+    End Sub
+
+    Private Sub BKurang_Click(sender As Object, e As EventArgs) Handles BKurang.Click
+        If tautologi() Then
+            TBLayar.Text += "-"
+        End If
+    End Sub
+
+    Private Sub BKali_Click(sender As Object, e As EventArgs) Handles BKali.Click
+        If tautologi() Then
+            TBLayar.Text += "x"
+        End If
+    End Sub
+
+    Private Sub BBagi_Click(sender As Object, e As EventArgs) Handles BBagi.Click
+        If tautologi() Then
+            TBLayar.Text += "÷"
+        End If
+    End Sub
+
+    Private Sub BSamaDengan_Click(sender As Object, e As EventArgs) Handles BSamaDengan.Click
+        soal = TBLayar.Text
+        soal = soal.Replace(",", ".")
+        hitungHasil()
+        TBLayar.Text = soal
+    End Sub
+    Sub hitungHasil()
+        bersihkan()
+        Dim l As Integer = soal.Length
+        For i As Integer = 0 To l - 1
+            soal = soal.Replace(",", ".")
+            If i >= l Then
+                Exit For
+            End If
+            If (soal(i) = "+") Then
+                hitung = Val(ambilKiri(i)) + Val(ambilKanan(i))
+
+                soal = kiri + hitung.ToString + kanan
+                i = 0
+                l = soal.Length
+                Debug.WriteLine("soal : " + soal)
+            ElseIf soal(i) = "-" Then
+                hitung = Val(ambilKiri(i)) - Val(ambilKanan(i))
+
+                soal = kiri + hitung.ToString + kanan
+                i = 0
+                l = soal.Length
+            End If
+        Next
+
+    End Sub
+    Function ambilKiri(index As Integer) As String
+        Dim i As Integer = 0
+        For i = index - 1 To 0 Step -1
+            If (soal(i) = "x" Or soal(i) = "÷" Or soal(i) = "+" Or soal(i) = "-") Then
+                i += 1
+                Exit For
+            End If
+        Next
+        If i < 0 Then
+            i = 0
+        End If
+
+        kiri = soal.Substring(0, i)
+        Return soal.Substring(i, index - i)
+    End Function
+    Function ambilKanan(index As Integer) As String
+        Dim i As Integer = 0
+        Dim panjang = soal.Length
+        Dim z As Integer = 1
+        For i = index + 1 To panjang - 1 Step 1
+            If (soal(i) = "x" Or soal(i) = "÷" Or soal(i) = "+" Or soal(i) = "-") Then
+                z -= 1
+                Exit For
+            End If
+        Next
+        If i > panjang - 1 Then
+            i = panjang - 1
+        End If
+        Debug.WriteLine("index : " + index.ToString)
+        Debug.WriteLine("i : " + i.ToString)
+        Debug.WriteLine("panjang : " + panjang.ToString)
+
+        kanan = soal.Substring(i, panjang - i - z)
+
+        Return soal.Substring(index + 1, i - index)
+
+    End Function
+    Sub bersihkan()
+        Dim l As Integer = soal.Length
+        For i As Integer = 0 To l - 1
+            soal = soal.Replace(",", ".")
+            Debug.WriteLine("soal repl = " + soal)
+            If i >= l Then
+                Exit For
+            End If
+            If (soal(i) = "÷") Then
+
+                hitung = Val(ambilKiri(i)) / Val(ambilKanan(i))
+
+                soal = kiri + hitung.ToString + kanan
+                i = 0
+                l = soal.Length
+                Debug.WriteLine("soal : " + soal)
+            ElseIf soal(i) = "x" Then
+                hitung = Val(ambilKiri(i)) * Val(ambilKanan(i))
+
+                soal = kiri + hitung.ToString + kanan
+                i = 0
+                l = soal.Length
+
+                Debug.WriteLine("soal : " + soal)
+            End If
+        Next
+    End Sub
+End Class
